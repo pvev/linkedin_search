11d498fb26429ca0a035714b1b6c3539fc74fb31
Pablo Velez <pvelez@bp-3.com>
2018-10-02 23:26:14 -0500

    Asincronous not working

--- index.js
+++ index.js
@@ -1,57 +1,91 @@
-const connection = require("./connection"); 
+const connection = require("./connection");
 
 const csv = require("fast-csv");
 const fs = require('fs');
-const linkedinPage = connection.connect();
+const linkedinConnection = connection.connect;
 
-var stream = fs.createReadStream("name_titles.1.csv");
+var contacts = [];
 
-const CREDS = [];
-CREDS['username'] = 'pablovv2016@gmail.com';
-CREDS['password'] = 'Osito123$';
+var stream = fs.createReadStream("test-to-populate.csv");
 
-var csvStream = csv.createWriteStream({ headers: true }),
-  writableStream = fs.createWriteStream("name_titles_updated2.csv");
-
-writableStream.on("finish", function () {
-  console.log("DONE WRITING!");
-});
-
-csvStream.pipe(writableStream);
-
-var parser = csv.fromStream(stream, { headers: true })
+  csv.fromStream(stream, { headers: true })
   .on("data", function (data) {
-    parser.pause();
-    searchPerson(data, () => {
-      parser.resume();
-      csvStream.write(data);
-    });
+    contacts.push(data);
   })
   .on("end", function () {
-    console.log("done reading");
-    csvStream.end();
-
+    console.log(contacts.length, " contacts parsed \n Starting LinkedIn Search!");
+    startSearch();
   });
 
-let searchPerson = async (person, callback) => {
+let writeCsv = () => {
+  var csvStream = csv.createWriteStream({ headers: true }),
+    writableStream = fs.createWriteStream("test-to-populate-updated.csv");
+
+  csvStream.pipe(writableStream);
+
+  contacts.forEach((contact) => {
+    csvStream.write(contact);
+  });
+
+  writableStream.on("finish", function () {
+    console.log("DONE WRITING!");
+    csvStream.end();
+  });
+}
+
+let startSearch = async () => {
+  let page = linkedinConnection();
+  contacts.forEach(async (contact) => {
+    // filter if empty name
+    let filter = filterContact(contact);
+    if (filter) {
+      return false;
+    }
+    // get the profile link
+    let link = '';
+    try {
+      link = await searchPersonAndGetLink(contact, page);
+      if (link == '') {
+        return false;
+      }
+      // Get person info
+      await updatePersonInfo(page, link, contact);
+    } catch (error) {
+      console.log('An error occurred while getting the link', error);
+      return false;
+    }
+  });
+  writeCsv();
+}
+
+let filterContact = (contact) => {
+  let filter = false;
+  if (contact.first_name == '' || contact.last_name == '') {
+    contact.log = 'Not Updated - empty name';
+    filter = true;
+  }
+  return filter;
+}
+
+let searchPersonAndGetLink = async (person, linkedinPage) => {
   linkedinPage.then(async (page) => {
     let query = getQueryString(person);
     let searchUrl = 'https://www.linkedin.com/search/results/all/?keywords=' + query + '&origin=GLOBAL_SEARCH_HEADER';
     await page.goto(searchUrl);
 
-    let possiblePeople = getPossiblePeople(page);
-    possiblePeople.then((links) => {
+    let personLinks = getPossiblePeople(page);
+
+    personLinks.then((links) => {
       if (links && links.length > 0) {
         if (links.length > 1) {
-          person.more_that_one = 'More than one found';
-          callback();
+          person.log = 'Not updated - More than one found';
         }
-        makeMatch(page, links, person, callback);
+        return links[0];
       } else {
-        person.found = 'Not found';
-        callback();
+        person.log = 'Not updated - Not found';
       }
     });
+    return '';
 
   });
 };
@@ -70,23 +104,17 @@ let getPossiblePeople = async (page) => {
   return peopleLinks;
 };
 
-let makeMatch = async (page, links, person, callback) => {
-  links.forEach(link => {
-    matchPerson(page, link, person, callback)
-  });
-}
-
 function getQueryString(person) {
   let query = '';
-  query += person.First_Name ? person.First_Name + ' ' : '';
-  query += person.Last_Name ? person.Last_Name : '';
-  query += person.Job_title ? ', ' + person.Job_title : '';
-  query += person.Company_Name ? ', ' + person.Company_Name : '';
+  query += person.first_name ? person.first_name + ' ' : '';
+  query += person.last_name ? person.last_name : '';
+  query += person.job_title ? ', ' + person.job_title : '';
+  query += person.company_name ? ', ' + person.company_name : '';
 
   return query;
 }
 
-async function matchPerson(page, link, person, callback) {
+async function updatePersonInfo(page, link, person) {
   await page.goto(link);
   await page.evaluate(() => {
     window.scrollBy(0, window.innerHeight);
@@ -96,20 +124,23 @@ async function matchPerson(page, link, person, callback) {
   const experience = await page.$eval('#experience-section', el => el.innerText);
   let jobInfo = experience.split(/\r?\n/);
   jobInfo = getCompanyAndTitle(jobInfo);
+  console.log(jobInfo);
 
-  // console.log(jobInfo);
   /* get the state */
   await page.waitForSelector('section.pv-profile-section');
   const profile = await page.$eval('section.pv-profile-section h3.pv-top-card-section__location', el => el.innerText);
   let profileInfo = profile.split(/\r?\n/);
   profileInfo = getCityAndState(profileInfo);
 
+  updatePersonInfo(jobInfo, profileInfo, person);
+}
+
+function updatePersonInfo(jobInfo, profileInfo, person) {
   person.Title_update = jobInfo.title;
   person.Company_update = jobInfo.company;
   person.City_update = profileInfo.city;
   person.State_update = profileInfo.state;
-  person.found = '';
-  callback();
+  person.log = 'Successfully updated';
 }
 
 function getCompanyAndTitle(jobInfo) {
@@ -143,24 +174,24 @@ function getCityAndState(info) {
 }
 
 //TODO confirm if is the person (loop over all companies and compare with company)
-function confirmPerson(jobInfo, person) {
-  let isThePerson = false;
-  jobInfo.forEach((info, index) => {
-    if (info === 'Company Name') {
-      if (jobInfo[index + 1].toLowerCase().trim().search(person.Job_title.toLowerCase().trim()) !== -1) {
-        isThePerson = true;
-      }
-    }
-  });
-  return isThePerson;
-}
+// function confirmPerson(jobInfo, person) {
+//   let isThePerson = false;
+//   jobInfo.forEach((info, index) => {
+//     if (info === 'Company Name') {
+//       if (jobInfo[index + 1].toLowerCase().trim().search(person.job_title.toLowerCase().trim()) !== -1) {
+//         isThePerson = true;
+//       }
+//     }
+//   });
+//   return isThePerson;
+// }
 
   // const fakePerson = {  
-  //     First_Name: 'Benito',  
-  //     Last_Name: 'Camelas',
+  //     first_name: 'Benito',  
+  //     last_name: 'Camelas',
   //     Email: 'estevan.dufrin@rigzone.comm',
-  //     Job_title: '',
-  //     Company_Name: 'Alert Logicas',
+  //     job_title: '',
+  //     company_name: 'Alert Logicas',
   //     Company_update: '',
   //     Title_update: '',
   //     City: '',
